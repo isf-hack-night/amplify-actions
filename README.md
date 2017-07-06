3be23e48a6ad10ec8877c67a7c950ad859f482f4
Peter Hosey <git@boredzo.org>
2017-07-06 09:03:28 -0700

    Initial prototype.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # amplify-actions
 Plug-in to embed actions from the Amplify back-end into a WordPress site.
+
+## Usage
+
+    amplify_actions_all('CA', 12);
+    
+This will produce a `div` element containing a list of all actions, with their call scripts and background info, for that state and district.
+
+The content is sourced from wp-content/amplify-actions-$state$district.json, which you must create/update yourself. Our plan is to have a cron job that downloads that content from Amplify to that file on a reasonable schedule.
---
+++ amplify-actions.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * @package Amplify_Actions
+ * @version 0.1
+ */
+/*
+Plugin Name: Amplify Actions
+Plugin URI: https://github.com/isf-hack-night/amplify-actions
+Description: Displays actions sourced from Amplify for a particular state/district.
+Author: Indivisible SF
+Version: 0.1
+Author URI: https://indivisiblesf.org/
+*/
+
+# https://michelf.ca/projects/php-markdown/
+require_once('Michelf/Markdown.inc.php');
+
+# CSS constants for use in content prefixes.
+define('BLACK_TELEPHONE', '\260e');
+define('EMAIL_SYMBOL', '\01f4e7');
+define('NO_BREAK_SPACE', '\00a0');
+
+function _amplify_present_one_phone_number($office) {
+	$telLink = 'tel:' . str_replace(' ', '', $office['phone']);
+	$officeDesc = ($office['state'] == 'DC') ? 'DC' : $office['city'];
+	print '<li class="amplify-contact-each amplify-contact-phone"><a href="' . htmlentities($telLink) . '">' . $officeDesc . '</a></li>' . "\n";
+}
+
+function _amplify_present_one_action($action) {
+	$script = $action['callScriptMd'];
+	if (!$script) {
+		$script = $action['flexBodyMd'];
+	}
+
+	print '<div class="amplify-action">' . "\n";
+	print '<h3 class="amplify-title"><span class="amplify-title-preTitle">' . htmlentities($action['preTitle']) . '</span> ' . htmlentities($action['title']) . '</h3>' . "\n";
+
+	print '<div class="amplify-callScript">' . str_replace('<h1>', '<h4>', str_replace('</h1>', '</h4>', \Michelf\Markdown::defaultTransform($script))) . '</div>' . "\n";
+
+	$person = isset($action['person']) ? $action['person'] : null;
+	if ($person) {
+		$emailLink = isset($person['emailFormUrl']) ? $person['emailFormUrl'] : null;
+		$phones = isset($person['offices']) ? $person['offices'] : [];
+		if ($emailLink or $phones) {
+			print '<div class="amplify-contact">' . "\n";
+			print '<ul class="amplify-contact-items">' . "\n";
+			array_map('_amplify_present_one_phone_number', $phones);
+			if ($emailLink) {
+				print '<li class="amplify-contact-each amplify-contact-email"><a href="' . htmlentities($emailLink) . '">' . 'Email</a></li>' . "\n";
+			}
+			print '</ul>';
+			print "</div>\n";
+		}
+	}
+
+	print "</div>\n";
+}
+
+function amplify_actions_all($state, $district) {
+	$district = 0 + $district;
+	if ($district < 0 || $district > 99) {
+		# That's not a district. (California has 53, the most in the Union, but let's be flexible here.)
+		return;
+	}
+	if (1 != preg_match('/^[A-Z][A-Z]$/', $state)) {
+		# That's not a state.
+		return;
+	}
+
+	$file = fopen("wp-content/amplify-actions-$state$district.json", 'r');
+	if (! $file) {
+		print "No actions today!\n";
+		return;
+	}
+
+	print '<div class="amplify-actions amplify-' . htmlentities($state) . '-' . htmlentities($district) . '">' . "\n";
+	print '<h2 class="amplify-actions-header">Actions for ' . htmlentities($state) . '-' . htmlentities($district) . '</h2>' . "\n";
+
+	$json = fread($file, 1000000);
+	$actions = json_decode($json, true);
+
+	array_map('_amplify_present_one_action', $actions['concreteActions']);
+
+	fclose($file);
+
+	print "</div>\n";
+}
+
+/*
+?><html>
+<head><meta charset="utf-8"><title>CA-12</title>
+<style type="text/css">
+.amplify-title-preTitle { font-weight: normal; }
+.amplify-contact-phone:before { content: "<?php print BLACK_TELEPHONE . NO_BREAK_SPACE; ?>" }
+.amplify-contact-email:before { content: "<?php print EMAIL_SYMBOL . NO_BREAK_SPACE; ?>" }
+</style></head>
+<body><?php
+amplify_actions_all('CA', 12);
+?>
+</body></html>
+*/
+?>
